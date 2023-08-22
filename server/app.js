ae0c51725c5760ae44d4c533f4981bf526e3a755
Arteks718 <artexbro.official@gmail.com>
2023-08-22 18:14:56 +0300

    Create server

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const appRouter = require("./router/index.js");
+const express = require("express");
+const cors = require("cors");
+const { errorHandlers } = require('./middleware')
+
+const app = express();
+app.use(cors({ origin: '*' }))
+app.use(express.json());
+
+app.use("/api", appRouter);
+app.use(errorHandlers.dbErrorHandler, errorHandlers.errorHandler)
+module.exports = app;
